827a7590d065c6f46bdef71bebe3d019036b38ef
Michael Zill <michael.zill@gmail.com>
2024-04-10 13:12:54 +0200

    Explicit memory.x

--- core1/src/main.rs
+++ core1/src/main.rs
@@ -14,7 +14,32 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Core1: STM32H755 Embassy HSEM Test.");
-    let p = embassy_stm32::init(Config::default());
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hsi = Some(HSIPrescaler::DIV1);
+        config.rcc.csi = true;
+        config.rcc.hsi48 = Some(Default::default()); // needed for RNG
+        config.rcc.pll1 = Some(Pll {
+            source: PllSource::HSI,
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL50,
+            divp: Some(PllDiv::DIV2),
+            divq: None,
+            divr: None,
+        });
+        config.rcc.sys = Sysclk::PLL1_P; // 400 Mhz
+        config.rcc.ahb_pre = AHBPrescaler::DIV2; // 200 Mhz
+        config.rcc.apb1_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.apb2_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.apb3_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.apb4_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.voltage_scale = VoltageScale::Scale1;
+        // Set SMPS power config otherwise MCU will not powered after next power-off
+        config.rcc.supply_config = SupplyConfig::DirectSMPS;
+    }
+
+    let p = embassy_stm32::init(config);
     info!("Config set");
 
     let mut led_yellow = Output::new(p.PE1, Level::Low, Speed::Low);
