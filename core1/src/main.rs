a7a09a6c75ff7b98e0240c58e1a1bd77d821bf6c
Michael Zill <michael.zill@gmail.com>
2024-04-17 14:53:56 +0200

    Michael/dual core (#3)
    * Initial dual-core support
    
    * Removed reference to local embassy
    
    * Removed unused reference
    
    * Improve VSCode configuration for multi core
    
    Includes patched yaml file with correct AP for core 1.
    
    * Add lib for HAL extensions
    
    * Fixed wrong test for core1 startup
    
    * Fixed merge error in chip config file
    
    * Fixed CI build error
    
    * Experimental dual-core working solution
    
    * Add  excluded projects to rust-analyzer
    
    * Enabled embassy timer on 2nd core
    
    * Explain why we need the HSEM2 IRQ handler
    
    * Refactored APIs for dual core support
    
    ---------
    
    Co-authored-by: Santiago Cingolani <santiago.cingolani@esrlabs.com>

--- core1/src/main.rs
+++ core1/src/main.rs
@@ -1,29 +1,50 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::{
-    gpio::{Level, Output, Speed},
-    Config,
-};
 use embassy_time::Timer;
+use hal::{
+    gpio::{Level, Output, Speed},
+    interrupt,
+};
 
-use {defmt_rtt as _, panic_probe as _};
+use {
+    defmt_rtt as _, embassy_stm32 as hal, embassy_stm32::pac, panic_probe as _,
+    stm32h7hal_ext as hal_ext,
+};
+
+// This function handles HSEM interrupt request only
+// during initial startup. This interriupt handler
+// gets replaced by the one in the HAL if the application
+// uses the HSEM peripheral.
+#[interrupt]
+#[allow(non_snake_case)]
+fn HSEM2() {
+    // FIXME: the semaphore ID is hardcoded
+    pac::HSEM.ier(1).write(|w| w.set_ise(0, false));
+    pac::HSEM.icr(1).write(|w| w.set_isc(0, true));
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    info!("Core1: STM32H755 Embassy HSEM Test.");
+    //info!("Core1: STM32H755 Embassy HSEM Test.");
 
-    let p = embassy_stm32::init(Config::default());
-    info!("Config set");
+    hal_ext::core1_startup();
+
+    let p = embassy_stm32::init_core1(200_000_000);
+
+    //info!("Config set");
+
+    // Enable the Cortex-M4 ART Clock
+    // pac::RCC.ahb1enr().modify(|w| w.set_arten(true));
 
     let mut led_yellow = Output::new(p.PE1, Level::Low, Speed::Low);
 
     loop {
         led_yellow.set_high();
-        Timer::after_millis(500).await;
+        Timer::after_millis(250).await;
+
         led_yellow.set_low();
-        Timer::after_millis(500).await;
+        Timer::after_millis(250).await;
     }
 }
